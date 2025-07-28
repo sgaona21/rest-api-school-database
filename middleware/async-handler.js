ab30fc10c39c1b692ae52e529d8760f19b1dccd7
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-07-28 15:44:19 -0700

    created middleware folder - Users routes functional but not complete

---
+++ middleware/async-handler.js
@@ -0,0 +1,10 @@
+// helper async function to wrap each route
+exports.asyncHandler = (cb) => {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,39 @@
+'use strict';
+
 const express = require('express');
+const { asyncHandler } = require('../middleware/async-handler');
+const { authenticateUser } = require('../middleware/authorize');
+const { User } = require('../models');
+
 const router = express.Router();
 
-// GET returns all fields for authenitcated user
-router.get('/users', (req, res, next) => {
-    
-})
+router.get('/', authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
+
+  res.json({
+    id: user.id,
+    name: user.firstName,
+    lastName: user.lastName,
+    emailAddress: user.emailAddress
+  });
+}));
+
+
+router.post('/', asyncHandler(async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.status(201).json({ "message": "Account successfully created!" });
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+}));
 
 
 module.exports = router;
+
+
