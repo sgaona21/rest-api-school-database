56120d1bd8a65e6405b31cd0d799177af87b1eca
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-07-29 07:30:21 -0700

    all exceeds phases complete
    added logic to allow only course creators to update/delete existing course

--- routes/courses.js
+++ routes/courses.js
@@ -7,18 +7,21 @@ const { Course } = require('../models');
 
 const router = express.Router();
 
+// GET ALL courses
 router.get('/', asyncHandler(async (req, res) => {
     const courses = await Course.findAll()
 
     res.status(200).json(courses);
 }));
 
+//GET a specific course
 router.get('/:id', asyncHandler(async (req, res) => {
     const course = await Course.findByPk(req.params.id);
 
     res.status(200).json(course);
 }));
 
+// CREATE a course
 router.post('/', authenticateUser, asyncHandler(async (req, res) => {
   try {
     await Course.create(req.body);
@@ -34,12 +37,17 @@ router.post('/', authenticateUser, asyncHandler(async (req, res) => {
   }
 }));
 
+// UPDATE a course 
 router.put('/:id', authenticateUser, asyncHandler(async (req, res) => {
   try {
     const course = await Course.findByPk(req.params.id);
 
-    await course.update(req.body);
+    if (course.userId === req.currentUser.id) {
+      await course.update(req.body);
     res.status(204).json({ "message": "Course successfully updated!" });
+    } else {
+      res.status(403).json({ message: 'You do not have permission to change this course.' });
+    }
   } catch (error) {
     if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
       const errors = error.errors.map(err => err.message);
@@ -50,12 +58,17 @@ router.put('/:id', authenticateUser, asyncHandler(async (req, res) => {
   }
 }));
 
+// DELETE a course 
 router.delete('/:id', authenticateUser, asyncHandler(async (req, res) => {
   try {
     const course = await Course.findByPk(req.params.id);
 
-    await course.destroy();
-    res.status(204).json({ "message": "Course successfully updated!" });
+    if (course.userId === req.currentUser.id) {
+      await course.destroy();
+      res.status(204).json({ "message": "Course successfully updated!" });
+    } else {
+      res.status(403).json({ message: 'You do not have permission to DELETE this course.' });
+    }
   } catch (error) {
     if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
       const errors = error.errors.map(err => err.message);
